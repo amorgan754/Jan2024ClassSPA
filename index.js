1fc3676c75dc674b73a62fe3b9b9e6cfb0a1454a
Ashley Morgan <aglass754@gmail.com>
2024-02-29 13:56:52 -0800

    Start turning application into SPA

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// imports
+import { Header, Nav, Main, Footer } from "./components";
+
+// render function
+function render() {
+  document.querySelector("#root").innerHTML = `
+      ${Header()}
+      ${Nav()}
+      ${Main()}
+      ${Footer()}
+    `;
+}
+
+render();
+
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
