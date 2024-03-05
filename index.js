739a19907a2342c33ec572aa745935df2e306506
Ashley Morgan <aglass754@gmail.com>
2024-03-05 09:13:59 -0800

    Create render function and router

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 // imports
 import { Header, Nav, Main, Footer } from "./components";
+import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
 
-// render function
-function render() {
+// router
+const router = new Navigo("/");
+
+// render
+function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
-      ${Header()}
-      ${Nav()}
-      ${Main()}
+      ${Header(state)}
+      ${Nav(store.Links)}
+      ${Main(state)}
       ${Footer()}
     `;
+  router.updatePageLinks();
+  afterRender();
 }
 
-render();
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
 
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+
+      if (view in store) {
+        render(store[view]);
+      } else {
+        render(store.Viewnotfound);
+        console.log(`View ${view} not defined`);
+      }
+    }
+  })
+  .resolve();
